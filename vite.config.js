e2c25aa6e80a905de9ee311d27b11529ca67f5d6
zhuo <3063529117@qq.com>
2026-04-03 18:10:15 +0800

    分环境配置base，解决本地+线上404

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  // 开发环境（npm run dev）base设为 /，生产环境（npm run build）设为 /zhuo.ys-blog/
+  const isProduction = mode === 'production'
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    },
+    // 关键：分环境配置base
+    base: isProduction ? '/zhuo.ys-blog/' : '/',
+    build: {
+      assetsDir: 'assets'
     }
-  },
-  base: '/zhuo.ys-blog/'
+  }
 })
